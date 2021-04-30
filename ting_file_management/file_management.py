e4baf770b1f57e31f9e1351d84741b8a96d108b7
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2021-04-30 19:55:49 -0300

    requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,17 @@
+import sys
+from os.path import isfile
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    """Aqui irá sua implementação
+    Tá """
+    try:
+        assert path_file.endswith('.txt')
+        with open(path_file, 'r') as file:
+            data = file.read().splitlines()
+    except AssertionError:
+        sys.stderr.write("Formato inválido\n")
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+    else:
+        return data
