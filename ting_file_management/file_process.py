ed62cbe54af1a35bd703e89e5a83d5347d4c46d7
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2021-04-30 21:28:19 -0300

    Requisito 4

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,35 @@
+import sys
+from ting_file_management.queue import Queue
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
     """Aqui irá sua implementação"""
+    data = txt_importer(path_file)
+    output = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(data),
+        "linhas_do_arquivo": data
+    }
+    sys.stdout.write(f"{output}\n")
+    instance.enqueue(output)
 
 
 def remove(instance):
     """Aqui irá sua implementação"""
+    if len(instance) == 0:
+        sys.stdout.write(f"Não há elementos\n")
+        return
+    remove = instance.dequeue()["nome_do_arquivo"]
+    sys.stdout.write(f"Arquivo {remove} removido com sucesso\n")
 
 
 def file_metadata(instance, position):
     """Aqui irá sua implementação"""
+
+
+if __name__ == "__main__":
+    proj = Queue()
+    process("statics/arquivo_teste.txt", proj)
+    
+    
